b127fed98df6f04e526792cd5b5ff6cb38860640
jmstfv <github@jmstfv.com>
2021-11-11 10:24:38 +0100

    Add custom green color
    Colorize labels to light green

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
       boxShadow: {
         'button': '.4rem .4rem 0 0 #adc2a9'
       },
+      colors: {
+        'green': {
+          'light': '#d3e4cd',
+          'DEFAULT': '#adc2a9',
+        },
+      }
     },
   },
   variants: {
