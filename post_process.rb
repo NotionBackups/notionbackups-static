8ac1635aed8c181272a951e1b4b1b79d654a7500
jmstfv <github@jmstfv.com>
2023-10-07 21:46:49 +0400

    Remove .html from feed URLs

--- post_process.rb
+++ post_process.rb
@@ -1,17 +1,25 @@
 # frozen_string_literal: true
 
-# Remove the .html extension from sitemap URLs
+# Remove the .html extension from sitemap and feed URLs
 
 require 'nokogiri'
 
 begin
-  xml = File.read('output/sitemap.xml')
-  doc = Nokogiri::XML(xml)
+  sitemap_xml = File.read('output/sitemap.xml')
+  sitemap_doc = Nokogiri::XML(sitemap_xml)
 
-  locs = doc.xpath('/ns:urlset/ns:url/ns:loc', 'ns' => 'http://www.sitemaps.org/schemas/sitemap/0.9')
+  locs = sitemap_doc.xpath('/ns:urlset/ns:url/ns:loc', 'ns' => 'http://www.sitemaps.org/schemas/sitemap/0.9')
   locs.each { |loc| loc.content = loc.content.gsub(/.html/, '') }
 
-  File.write('output/sitemap.xml', doc.to_xml)
+  File.write('output/sitemap.xml', sitemap_doc.to_xml)
+
+  atom_xml = File.read('output/guides/feed.xml')
+  atom_doc = Nokogiri::XML(atom_xml)
+
+  hrefs = atom_doc.xpath('/ns:feed/ns:entry/ns:link[@rel="alternate"]', 'ns' => "http://www.w3.org/2005/Atom")
+  hrefs.each { |href| href.set_attribute("href", href.attr("href").gsub(/.html/, '')) }
+
+  File.write('output/guides/feed.xml', atom_doc.to_xml)
 rescue SystemCallError => e
   puts e.message
 end
